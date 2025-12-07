3e50278d4df674193c4c76dc724b892b6c086748
shinde6152 <shindepranav6304.com>
2025-12-07 22:35:09 +0530

    remove the unwanted modal

--- employee_management/employee/admin.py
+++ employee_management/employee/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import employee_data
 from .models.user import user
 # Register your models here.
 
-admin.site.register(employee_data)
 admin.site.register(user)
